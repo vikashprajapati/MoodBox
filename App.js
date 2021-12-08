047da65af670f01a060d34149e7842c388897b55
vikash <prajapativikash0000@gmail.com>
2021-12-08 23:52:54 +0530

    added basic images and text at the initial screen

--- App.js
+++ App.js
@@ -7,105 +7,80 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
+  Image,
+  Text,
+  TouchableOpacity,
+  Button,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.darker : '#ffffff',
+    height: '100%',
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={styles.col}>
+        <Image
+          source={require('./assets/illustration.jpg')}
+          style={{width: 350, height: 350}}
+        />
+        <Text style={styles.heading}>Time to meditate</Text>
+        <Text style={styles.heading_2}>
+          Take a breath, {'\n'} and ease your mind
+        </Text>
+        <TouchableOpacity activeOpacity={0.95} style={styles.button}>
+          <Text style={styles.text}>Let's get started</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  col: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  heading: {
+    fontSize: 26,
+    color: Colors.darker,
+    fontWeight: 'bold',
+    paddingTop: 32,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  heading_2: {
+    fontSize: 20,
+    color: '#aaa',
+    textAlign: 'center',
+    paddingVertical: 16,
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    flexDirection: 'row',
+    height: 60,
+    width: '80%',
+    backgroundColor: '#82bf79',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+    elevation: 3,
+    borderRadius: 12,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#ffffff',
   },
 });
 
